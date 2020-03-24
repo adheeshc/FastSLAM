2ec038dbb9f43ce21eba109d269d840b8420334b
Adheesh <adheeshchat@gmail.com>
2020-03-24 19:51:05 -0400

    init commit

---
+++ aux.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#   ___      _ _                    _     
+#  / _ \    | | |                  | |    
+# / /_\ \ __| | |__   ___  ___  ___| |__  
+# |  _  |/ _` | '_ \ / _ \/ _ \/ __| '_ \ 
+# | | | | (_| | | | |  __/  __/\__ \ | | |
+# \_| |_/\__,_|_| |_|\___|\___||___/_| |_|
+# Date:   2020-03-24 19:50:39
+# Last Modified time: 2020-03-24 19:50:47
+
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import math
+import random
+
